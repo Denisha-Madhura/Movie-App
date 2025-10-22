4da549355c5b52b592f766801ceb54aa42b08160
denisha-madhura <denisha.madhura2007@gmail.com>
2025-10-22 13:56:33 +0530

    retrying for github pages

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const repoName = 'Movie-App';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: `/${repoName}/`,
+  plugins: [react(), tailwindcss()],
+})
